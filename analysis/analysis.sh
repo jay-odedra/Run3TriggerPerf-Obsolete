34a48e82d2fc99fbc36831cca66934550aa304fa
Jay Odedra <jodedra@lxplus713.cern.ch>
2022-04-20 00:39:04 +0200

    for charmonium data

--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export X509_USER_PROXY=$1
 
-cd /eos/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis
+cd /afs/cern.ch/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis
 
 eval `scramv1 runtime -sh`
 
-python3 analysis.py @/eos/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis/outputcharmonium0"$2".txt -o puritycheckcharm"$2".root
+python3 analysis.py @/afs/cern.ch/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis/outputcharmonium"$2".txt -o puritycheckcharm"$2".root
 
-mv puritycheckcharm"$2".root /eos/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis/charmoniumoutputs/
+mv puritycheckcharm"$2".root /afs/cern.ch/user/j/jodedra/charmoniumanalysis/CMSSW_12_1_0/src/Run3TriggerPerf-1/analysis/charmoniumoutputs/
