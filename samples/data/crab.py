70843c1f7911c6e206b1543f5d467e51a0d0abc6
Jay Odedra <jayodedra1998@gmail.com>
2022-06-27 14:49:32 +0200

    eddited crab.py

--- samples/data/crab.py
+++ samples/data/crab.py
@@ -5,7 +5,7 @@ config.General.requestName = ''
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'hltDatapixeldoublet_v2.py'
+config.JobType.psetName = 'hltdevpath.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.numCores = 4
 
@@ -15,26 +15,25 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 #config.Data.totalUnits=10
 
-config.Data.outLFNDirBase = '/store/user/jodedra/Charmonium_20223003/' #% (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/jodedra/Bparkinghltpixeldoublet_20222806/' #% (getUsernameFromSiteDB())
 config.Data.publication = True
-config.Data.outputDatasetTag = 'Spring22_Trigger_20223003_MiniAODHLTRAWCOMB'
+config.Data.outputDatasetTag = 'Summer22_BParking_20222806_MiniAODHLTRAWCOMB'
 #config.Site.storageSite = 'T3_CH_PSI'
-config.Site.storageSite = 'T2_CH_CSCS'
+config.Site.storageSite = 'T2_UK_London_IC'
 #config.Site.ignoreGlobalBlacklist = True
 #config.Data.ignoreLocality = True
 #config.Site.whitelist = ['T2_CH_*']
 #config.Site.blacklist = ['T2_US_Purdue']
 
 
-config.General.workArea = 'crab_CMSSW12_1_0_30Mar'
+config.General.workArea = 'crab_CMSSW12_1_0_28Jun'
 
-name = 'Charmonium'
+name = 'Bparkinghltpixeldoublet'
 #name = 'ZeroBias10'
 
 config.General.requestName = name
-config.Data.inputDataset = '/' + name + '/Run2018D-12Nov2019_UL2018-v1/MINIAOD'
-config.Data.secondaryInputDataset = '/' + name + '/Run2018D-v1/RAW'
-
+config.Data.inputDataset = '/ParkingBPH5/Run2018B-05May2019-v2/MINIAOD'
+config.Data.secondaryInputDataset = '/ParkingBPH5/Run2018B-v1/RAW'
 
 #for a in [1,2,3,4,5,6,7]:
     
@@ -56,6 +55,14 @@ config.Data.secondaryInputDataset = '/' + name + '/Run2018D-v1/RAW'
 #'/EphemeralZeroBias6/Run2018D-v1/RAW'
 #'/EphemeralZeroBias7/Run2018D-v1/RAW'
 #'/EphemeralZeroBias8/Run2018D-v1/RAW'
+
+
+
+
+#config.General.requestName = 'EphemeralZeroBias3'
+#config.Data.inputDataset = '/EphemeralZeroBias3/Run2018D-PromptReco-v2/MINIAOD'
+#config.Data.secondaryInputDataset = '/EphemeralZeroBias3/Run2018D-v1/RAW'
+
 
 
 
